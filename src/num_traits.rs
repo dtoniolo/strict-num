0fb996b53c3eb4437e8cec06e2e858b2316725de
dtoniolo <54995110+dtoniolo@users.noreply.github.com>
2024-04-21 11:38:12 +0200

    Implements `CheckedSub`

--- src/num_traits.rs
+++ src/num_traits.rs
@@ -2,8 +2,8 @@
 
 use super::FiniteF32;
 
-use core::ops::{Add, Sub};
-use num_traits::ops::checked::CheckedAdd;
+use core::ops::{Add, Neg, Sub};
+use num_traits::ops::checked::{CheckedAdd, CheckedSub};
 
 impl Add for FiniteF32 {
     type Output = Self;
@@ -30,9 +30,20 @@ impl Sub for FiniteF32 {
     type Output = Self;
 
     /// # Panics
-    /// This function will panic if and only if the result overflows.
+    /// This function will panic if and only if the result overflows. See
+    /// [`Self::checked_sub`] for a version without panic.
     fn sub(self, other: Self) -> Self::Output {
         Self::new(self.get() - other.get()).expect("Overflowed when subtracting two real numbers.")
+    }
+}
+
+impl CheckedSub for FiniteF32 {
+    /// Performs the subtracton between `self` and `other` and returns [`Some`] if and
+    /// only if the result is finite.
+    ///
+    /// This function never panics.
+    fn checked_sub(&self, other: &Self) -> Option<Self> {
+        self.checked_add(&other.neg())
     }
 }
 
@@ -40,7 +51,10 @@ impl Sub for FiniteF32 {
 mod tests {
     use super::FiniteF32;
 
-    use num_traits::{identities::Zero, ops::checked::CheckedAdd};
+    use num_traits::{
+        identities::Zero,
+        ops::checked::{CheckedAdd, CheckedSub},
+    };
     use proptest::{
         num::f32::{INFINITE, NEGATIVE, POSITIVE},
         prelude::any,
@@ -160,6 +174,45 @@ mod tests {
         #[should_panic]
         fn test_sub_overflow(finite in gen_finite(), infinite in gen_infinite()) {
             let _ = finite - FiniteF32(infinite);
+        }
+    }
+
+    proptest! {
+        /// Test that the checked subtraction of zero to a [`FiniteF32`] returns the
+        /// original number wrapped in [`Some`].
+        ///
+        /// Moreover, test that the checked subtraction of a [`FiniteF32`] returns the
+        /// opposite of the original number wrapped in [`Some`].
+        #[test]
+        fn test_checked_sub_zero(x in gen_finite()) {
+            let result = x.checked_sub(&FiniteF32::new(0.0).unwrap());
+            assert_eq!(result, Some(x));
+            let result = FiniteF32::new(0.0).unwrap().checked_sub(&x);
+            assert_eq!(result, Some(FiniteF32::new(-x.get()).unwrap()));
+        }
+    }
+
+    proptest! {
+        /// Test that the checked subtraction of `self` to a [`FiniteF32`] returns
+        /// <code>[Some]([FiniteF32::zero]())</code>.
+        #[test]
+        fn test_checked_sub_self(x in gen_finite()) {
+            let result = x.checked_sub(&x);
+            assert_eq!(result, Some(FiniteF32::zero()));
+        }
+    }
+
+    proptest! {
+        /// Test that the checked subtraction of two [`FiniteF32`]s returns [`None`] when it
+        /// overflows.
+        ///
+        /// In order to guarantee overflow, one of the two [`FiniteF32`] actually stores an
+        /// infinite value.
+        #[test]
+        fn test_checked_sub_overflow(finite in gen_finite(), infinite in gen_infinite()) {
+            let infinite = FiniteF32(infinite);
+            assert_eq!(finite.checked_add(&infinite), None);
+            assert_eq!(infinite.checked_add(&finite), None);
         }
     }
 }

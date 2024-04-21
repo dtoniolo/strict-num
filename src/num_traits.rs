a8e8c4cffa2c1e9504f709a1f8686f6bf04aaddd
dtoniolo <54995110+dtoniolo@users.noreply.github.com>
2024-04-21 11:38:11 +0200

    Implements `Add` for `FiniteF32`

--- src/num_traits.rs
+++ src/num_traits.rs
@@ -1,1 +1,16 @@
 //! This module implements various traits defined in [`num_traits`] for our types.
+
+use super::FiniteF32;
+
+use core::ops::Add;
+
+impl Add for FiniteF32 {
+    type Output = Self;
+
+    /// # Panics
+    /// This function will panic if and only if the result overflow. See
+    /// [`Self::checked_add`] for a version without panic.
+    fn add(self, other: Self) -> Self::Output {
+        Self::new(self.get() + other.get()).expect("Overflowed when adding two real numbers.")
+    }
+}
